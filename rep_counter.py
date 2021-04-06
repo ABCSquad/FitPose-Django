6ae70d4bc5b2d2e8616fcd7de66732400d6a6c50
krantheman <akash.akasht.tom@gmail.com>
2021-04-07 01:45:46 +0530

    Added interactive plot for reps vs time and made some other changes.

--- rep_counter.py
+++ rep_counter.py
@@ -1,33 +1,57 @@
 # Imports
 import time
 
+
+# HELPER FUNCITONS -
+# These functions are called and used within or surrounding the exercise specific functions to aid them in carrying out their different operations.
+
+
 # Initializing variables related to reps in a dict
 def initialize_reps(reps):
     reps['count'] = 0
     reps['flag'] = -1
+    reps['timestamp'] = {}
     reps['time'] = {}
     reps['correct_form'] = {}
     reps['wrong_form'] = {}
 
-# For counting no. of frames in correct or wrong form for each rep
-def checkForm(dict, key):
-    if key in dict:
-        dict[key] += 1
-    else:
-        dict[key] = 0
+
+# For initializing correct_form and wrong_form dicts for each succesful rep 
+def check_key(dict1, dict2, key):
+    if key not in dict1:
+        dict1[key] = 0
+    if key not in dict2:
+        dict2[key] = 0
+
+
+# For converting starting timestamps of reps to time taken for each rep
+def get_rep_time(reps):
+    for i in range(1, reps['count']):
+        reps['time'][i] = reps['timestamp'][i+1] - reps['timestamp'][i]
+
+
+# EXERCISE SPECIFIC FUNCITONS - 
+# These functions are directly called by the main program depending on the exercise that the user selects and wishes to perform.
+
 
 # Counting reps for ohp
 def ohp_reps(right_deviation, left_deviation, right_shoulder_angle, left_shoulder_angle, reps):
 
+    check_key(reps['correct_form'],reps['wrong_form'],reps['count'])
+
     if right_deviation < 15 and left_deviation < 15:
+        reps['correct_form'][reps['count']] += 1
 
         if right_shoulder_angle < 90 and left_shoulder_angle < 90 and reps['flag'] == 0:
             reps['flag'] = 1
             reps['count'] += 1
-            reps['time'][reps['count']] = time.time()
+            reps['timestamp'][reps['count']] = time.time()
 
         elif right_shoulder_angle > 170 and left_shoulder_angle > 170 and (reps['flag'] == 1 or reps['flag'] == -1):
             reps['flag'] = 0
+
+    else:
+        reps['wrong_form'][reps['count']] += 1
 
     return reps
 
@@ -35,18 +59,20 @@ def ohp_reps(right_deviation, left_deviation, right_shoulder_angle, left_shoulde
 # Counting reps for bicep curls
 def curl_reps(shoulder_angle, elbow_angle, reps):
 
+    check_key(reps['correct_form'],reps['wrong_form'],reps['count'])
+
     if shoulder_angle<13 or shoulder_angle>355: 
-        checkForm(reps['correct_form'], reps['count'])
+        reps['correct_form'][reps['count']] += 1
 
         if elbow_angle > 160 and reps['flag'] == 0:
             reps['flag'] = 1
             reps['count'] += 1
-            reps['time'][reps['count']] = time.time()
+            reps['timestamp'][reps['count']] = time.time()
 
         elif elbow_angle < 65 and (reps['flag'] == 1 or reps['flag'] == -1):
             reps['flag'] = 0
 
     else:
-        checkForm(reps['wrong_form'], reps['count'])
+        reps['wrong_form'][reps['count']] += 1
             
     return reps
