d96b390e8288142df9716fe2987d72ba3cd566c9
BryanRonad <bryanronad@gmail.com>
2021-04-13 13:59:11 +0530

    For my beautiful akash

--- pose.py
+++ pose.py
@@ -14,7 +14,7 @@ import custom_pose
 reps = {}
 stats_dict = {}
 
-def main_pose(stats_dict, reps, exercise_name, side, exit_rep_count):
+def main_pose(stats_dict, reps, exercise_name, side="right", exit_rep_count=5000):
     
     initialize_reps(reps)
     initialize_stats(stats_dict)
